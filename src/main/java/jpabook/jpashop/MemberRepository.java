f8b5582e3c2187cc7e153f98f3c22c763ce49482
kjennn <jijo05037@naver.com>
2021-12-11 23:35:32 +0900

    first commit plz

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;   //그래들에서 설정할때 엔티티매니저 생성 자동으로 해줌
+
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}
