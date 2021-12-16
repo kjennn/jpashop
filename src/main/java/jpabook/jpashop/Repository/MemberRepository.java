af693284f70bb8ec11c2826fdfd846a0b60f22b8
kjennn <jijo05037@naver.com>
2021-12-16 15:22:59 +0900

    4th commit
    : 회원 레포지토리/회원 서비스 개발
    
    -- 지금까지 한 내용
    프로젝트 생성, h2 데이터베이스 설치, jpa와 db 설정, 도메인 모델과 테이블 설계 확인 후. 엔티티 클래스 개발

---
+++ src/main/java/jpabook/jpashop/Repository/MemberRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.Repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em; //그래들에서 설정할때 엔티티매니저 생성 자동으로 해줌
+
+    public void save(Member member){
+        em.persist(member); //insert query 날라감
+    }
+
+    public Member findOne(Long id){
+        return  em.find(Member.class, id); //단건 조회
+    }
+
+    public List<Member> findAll(){ // jpaql 사용,
+      return  em.createQuery("select m from Member m", Member.class)
+                 .getResultList();
+    }
+
+    public List<Member> findByName(String name){ //파라미터 바인딩해서 특정 이름에 대해 조회
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
---
+++ src/test/java/jpabook/jpashop/Service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.Service;
+
+import jpabook.jpashop.Repository.MemberRepository;
+import jpabook.jpashop.domain.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+
+    @Test
+   // @Rollback(value = false) 데이터 확인 가능
+    public void 회원가입() throws Exception{
+
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        em.flush();
+        assertEquals(member, memberRepository.findOne(saveId));
+
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception{
+
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        //예외발생  이름 같을시
+    }
+}
